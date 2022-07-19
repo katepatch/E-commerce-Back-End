26225ef1f9f7bdcb18a15f9a6934b67e04e58f76
kate patch <kate.epatch@gmail.com>
2022-07-19 00:36:22 -0600

    added get for category id

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -22,9 +22,23 @@ router.get('/', async (req, res) => {
 
 // find one category by its `id` value
   // be sure to include its associated Products
-router.get('/:id', (req, res) => {
-  
-});
+router.get('/:id', async (req, res) => {
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{ 
+        model: Product, 
+      attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+    }]
+    });
+    if(!categoryData) {
+      res.status(404).json({ message: 'No category with this id!'});
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+  });
 
 // create a new category
 router.post('/', (req, res) => {
