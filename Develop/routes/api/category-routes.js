85dd44a13608a3547cb596f6007bc8ba9d5bb44f
kate patch <kate.epatch@gmail.com>
2022-07-19 00:29:27 -0600

    added get for all categories

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -5,8 +5,19 @@ const { Category, Product } = require('../../models');
 
 // find all categories
   // be sure to include its associated Products
-router.get('/', (req, res) => {
-  
+router.get('/', async (req, res) => {
+  try {
+    const categoryData = await Category.findAll({
+      attributes: ['id', 'category_name'],
+      include: [{
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+      }]
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 // find one category by its `id` value
