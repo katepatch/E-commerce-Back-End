c7be617b1d64a1190521f8362d6de2f59d6dac5a
kate patch <kate.epatch@gmail.com>
2022-07-18 22:27:29 -0600

    added post to create new tag

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -34,8 +34,13 @@ router.get('/:id', async (req, res) => {
 });
 
 // create a new tag
-router.post('/', (req, res) => {
-  
+router.post('/', async (req, res) => {
+  try {
+    const tagData = await Tag.create(req.body);
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 // update a tag's name by its `id` value
