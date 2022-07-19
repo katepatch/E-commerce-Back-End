c9ade9e973ffbaf916bf3bca93b26446103a9b6a
kate patch <kate.epatch@gmail.com>
2022-07-18 22:25:48 -0600

    added get route for single tag  hope it works

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -17,8 +17,20 @@ router.get('/', async (req, res) => {
 
 // find a single tag by its `id`
   // be sure to include its associated Product data
-router.get('/:id', (req, res) => {
-  
+router.get('/:id', async (req, res) => {
+  try {
+    const tagData = await Tag.findByPk(req.params.id, {
+      include: [{ model: Product, through: ProductTag, as: 'tags'}]
+    });
+
+    if (!tagData) {
+      res.status(400).json({ message: 'No tag found with this id!'});
+      return;
+    }
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create a new tag
