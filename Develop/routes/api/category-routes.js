25efcdb1e207c19d458c896ca2add50fec809d79
kate patch <kate.epatch@gmail.com>
2022-07-19 00:46:10 -0600

    created update put for category route.  might change the syntax i have written for the tag route

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -51,8 +51,22 @@ router.post('/', async (req, res) => {
 });
 
 // update a category by its `id` value
-router.put('/:id', (req, res) => {
-  
+router.put('/:id', async (req, res) => {
+  try {
+    const categoryData = await Category.update(
+      {
+        category_name: req.body.category_name,
+      },
+      {
+        where: {
+          id: req.params.id
+        }
+      },
+    )
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 // delete a category by its `id` value
