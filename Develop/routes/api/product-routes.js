cdabdd4ecaab37fa53834bda2f3fe45e6773fa94
kate patch <kate.epatch@gmail.com>
2022-07-19 01:17:15 -0600

    added delete route for product

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -125,7 +125,20 @@ router.put('/:id', (req, res) => {
 
 // delete one product by its `id` value
 router.delete('/:id', (req, res) => {
-  
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    if(!productData) {
+      res.status(404).json({ message: 'No Product found with this id!' });
+      return;
+    }
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
