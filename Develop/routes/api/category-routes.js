fa2edbe9fb1f3059a57328a39d903598a4c40163
kate patch <kate.epatch@gmail.com>
2022-07-19 00:41:54 -0600

    created post for category routes

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -41,8 +41,13 @@ router.get('/:id', async (req, res) => {
   });
 
 // create a new category
-router.post('/', (req, res) => {
-  
+router.post('/', async (req, res) => {
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 // update a category by its `id` value
