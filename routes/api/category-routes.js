ad460724a8a1d33a0230bd08d27118f285ec5ba4
kate patch <kate.epatch@gmail.com>
2022-07-22 00:14:55 -0600

    cleaned up code in category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -42,12 +42,7 @@ router.get('/:id', async (req, res) => {
 
 // create a new category
 router.post('/', async (req, res) => {
-  // try {
-  //   const categoryData = await Category.create(req.body);
-  //   res.status(200).json(categoryData);
-  // } catch (err) {
-  //   res.status(400).json(err);
-  // }
+
   Category.create({
     category_name: req.body.category_name
   })
